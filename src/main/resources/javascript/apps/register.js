e655bdeca84329917ab18b496dff5ba62718c3bc
Florent BOURASSE <fbourasse@jahia.com>
2020-10-20 16:27:37 +0200

    Addition of menu entry at the server level.

--- src/main/resources/javascript/apps/register.js
+++ src/main/resources/javascript/apps/register.js
@@ -2,7 +2,16 @@ window.jahia.i18n.loadNamespaces('support-token-authentication-valve');
 
 window.jahia.uiExtender.registry.add('adminRoute', 'supportTokenAuthenticationValve', {
     targets: ['administration-sites:10'],
+    requiredPermission: 'siteAdminUsers',
     label: 'support-token-authentication-valve:label',
     isSelectable: true,
     iframeUrl: window.contextJsParameters.contextPath + '/cms/editframe/default/$lang/sites/$site-key.manageUsersTokens.html'
+});
+
+window.jahia.uiExtender.registry.add('adminRoute', 'supportTokenAuthenticationValveServer', {
+    targets: ['administration-server-usersAndRoles:10'],
+    requiredPermission: 'adminUsers',
+    label: 'support-token-authentication-valve:label',
+    isSelectable: true,
+    iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/$lang/settings.manageUsersTokens.html'
 });
